8c4662c40664a5ee04dd14aae90fb5fb8df10b99
Bruno Lins <linslimabruno@gmail.com>
2023-09-07 18:46:10 -0300

    Estilizando a sidebar

--- src/containers/Siderbar/index.tsx
+++ src/containers/Siderbar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+import { BotaoTema, Descricao, SidebarContainer } from "./styles"
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/linsbruno.png" alt="" />
-    <Titulo fontSize={20}>Bruno Lins</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Bruno Lins</Titulo>
+      <Paragrafo fontSize={16} tipo="secundario">
+        linsbruno
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
