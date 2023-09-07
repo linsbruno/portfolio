9f3cbea54d681a89b8a1ab1c6e6bc729671a979d
Bruno Lins <linslimabruno@gmail.com>
2023-09-07 20:33:37 -0300

    terminando o styledcomponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from "styled-components"
+
 import EstiloGlobal, { Container } from "./styles"
 import Sidebar from "./containers/Siderbar"
 import Sobre from "./containers/sobre"
 import Projetos from "./containers/Projetos"
+import temaLight from "./themes/light"
+import temaDark from "./themes/dark"
+import { useState } from "react"
 
 function App() {
+  const [useDarkTheme, setUseDarkTheme] = useState(false)
+
+  function changeTheme() {
+    setUseDarkTheme(!useDarkTheme)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={useDarkTheme ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={changeTheme} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
